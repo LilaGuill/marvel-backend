5a62b1cce902f16d3f28cbe45d0aa223527c873d
Lila Guillermic <lila.guillermic@gmail.com>
2020-02-19 15:59:04 +0100

    route comics

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.use(formidableMiddleware());
 app.use(cors());
 require("dotenv").config();
 
+app.post("/comics", async (req, res) => {
+  const hash = generateHash();
+  const id = req.fields.id;
+
+  const response = await axios.get(
+    `https://gateway.marvel.com:443/v1/public/comics?characters=${id}&ts=1&apikey=9a1739125a0fc37b9fd7ef0a9fb06592&hash=${hash}`
+  );
+  console.log(response.data.data);
+  res.json({ message: "ok" });
+});
+
 app.get("/", async (req, res) => {
   const hash = generateHash();
   console.log(hash);
@@ -15,9 +26,7 @@ app.get("/", async (req, res) => {
     "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=9a1739125a0fc37b9fd7ef0a9fb06592&hash=" +
       hash
   );
-  console.log(response.data.data);
-
-  res.json({ message: "ok" });
+  res.json(response.data.data);
 });
 
 app.all("*", (req, res) => {
