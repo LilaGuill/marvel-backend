771d2e666980c278e4287db608ce36ba1c6dbb74
Lila Guillermic <lila.guillermic@gmail.com>
2020-02-20 21:55:18 +0100

    marvel

--- routes/marvel.js
+++ routes/marvel.js
@@ -6,6 +6,33 @@ const axios = require("axios");
 const generateHash = require("../utils/generateHash");
 const generateTs = require("../utils/generateTs");
 
+router.post("/comics", async (req, res) => {
+  try {
+    const ts = generateTs();
+    const hash = generateHash(ts);
+    const id = req.fields.id;
+    const page = req.fields.page;
+    const limit = 100;
+    const offset = limit * page - 100;
+
+    if (req.fields.id != 0) {
+      const response = await axios.get(
+        `http://gateway.marvel.com/v1/public/comics?orderBy=title&characters=${id}&ts=${ts}&apikey=${process.env.MARVEL_PUBLIC_KEY}&hash=${hash}`
+      );
+      res.json(response.data.data);
+    } else {
+      const response = await axios.get(
+        `http://gateway.marvel.com/v1/public/comics?orderBy=title&limit=${limit}&offset=${offset}&ts=${ts}&apikey=${process.env.MARVEL_PUBLIC_KEY}&hash=${hash}`
+      );
+      res.json(response.data.data);
+    }
+
+    console.log(response.data.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 router.post("/search/charactere", async (req, res) => {
   try {
     const ts = generateTs();
@@ -39,33 +66,6 @@ router.post("/search/comics", async (req, res) => {
     );
 
     res.json(response.data.data);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-router.post("/comics", async (req, res) => {
-  try {
-    const ts = generateTs();
-    const hash = generateHash(ts);
-    const id = req.fields.id;
-    const page = req.fields.page;
-    const limit = 100;
-    const offset = limit * page - 100;
-
-    if (req.fields.id != 0) {
-      const response = await axios.get(
-        `http://gateway.marvel.com/v1/public/comics?orderBy=title&characters=${id}&ts=${ts}&apikey=${process.env.MARVEL_PUBLIC_KEY}&hash=${hash}`
-      );
-      res.json(response.data.data);
-    } else {
-      const response = await axios.get(
-        `http://gateway.marvel.com/v1/public/comics?orderBy=title&limit=${limit}&offset=${offset}&ts=${ts}&apikey=${process.env.MARVEL_PUBLIC_KEY}&hash=${hash}`
-      );
-      res.json(response.data.data);
-    }
-
-    console.log(response.data.data);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
